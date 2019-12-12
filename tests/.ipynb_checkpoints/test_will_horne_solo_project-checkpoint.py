20d28131e83d686de4e677ef384409ed2e69767c
William Horne <55260697+DataChicken77@users.noreply.github.com>
2019-12-12 18:23:49 -0500

    Updated test script; removed local_functions.py and added whs package.

--- tests/.ipynb_checkpoints/test_will_horne_solo_project-checkpoint.py
+++ tests/.ipynb_checkpoints/test_will_horne_solo_project-checkpoint.py
@@ -1,5 +1,5 @@
 from will_horne_solo_project import __version__
-from will_horne_solo_project import local_functions as lf
+import will_horne_solo_project as whs
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,13 +24,13 @@ def trimmed_file(loaded_file):
     trim_index = [9, 24]
     sec_set_toggle = 2
     triplicate = True
-    trimmed_df = lf.trim_excel(loaded_file, trim_index, sec_set_toggle)
+    trimmed_df = whs.clean.trim_excel(loaded_file, trim_index, sec_set_toggle)
     return trimmed_df
 
 @pytest.fixture
 def cleaned_file(trimmed_file):
     '''Gets loaded file to cleaned_df stage of main function; test effect of clean_excel().'''
-    cleaned_df = lf.clean_excel(trimmed_file)
+    cleaned_df = whs.clean.clean_excel(trimmed_file)
     cleaned_df = cleaned_df.replace(0, np.nan)
     return cleaned_df
 
@@ -42,14 +42,15 @@ def converted_file(cleaned_file):
     conversions = 1 / (10 ** (dilutions + plate_factor))
     converted_df = cleaned_file.mul(conversions, axis='columns')
     return converted_df
-    
+
+
+
 def test_read_blank_space(loaded_file):
     """
     If file loads correctly from read_excel(), there should be a space of Nan values in the data frame from (1,0) to (2, 5).
     The .count() method should, therefore, return 0 for this region.
     """
     assert loaded_file.iloc[1:2, 0:5].count().sum() == 0
-    assert 0
 
 def test_name_extraction(loaded_file):
     """
@@ -62,14 +63,12 @@ def test_name_extraction(loaded_file):
     file_strain_name = strain_name.replace('. ', '-').replace(' ', '_')
     assert strain_name == 'S. cerevisiae EXF-6761'
     assert file_strain_name == 'S-cerevisiae_EXF-6761'
-    assert 0
     
 def test_trimming(trimmed_file):
     """
     Tests that trim_excel() creates a data frame of the right size.
     """
     assert len(trimmed_file.index) == 9
-    assert 0
 
 def test_cleaning(cleaned_file, trimmed_file):
     """
@@ -77,7 +76,6 @@ def test_cleaning(cleaned_file, trimmed_file):
     """
     assert len(trimmed_file.index) == (len(cleaned_file.index) + 3)
     assert len(trimmed_file.columns) == (len(cleaned_file.columns) + 6)
-    assert 0
     
 def test_conversion(cleaned_file, converted_file):
     """
@@ -86,6 +84,4 @@ def test_conversion(cleaned_file, converted_file):
     """
     test_locs = [[0,0,10000000], [4,7,100000], [3,49,1000000]]
     for loc_v in test_locs:
-        assert converted_file.iloc[loc_v[0],loc_v[1]] == (cleaned_file.iloc[loc_v[0],loc_v[1]] * loc_v[2])
-        assert converted_file.iloc
-        assert converted_file.iloc
+        assert round(converted_file.iloc[loc_v[0],loc_v[1]]) == round(cleaned_file.iloc[loc_v[0],loc_v[1]] * loc_v[2])
